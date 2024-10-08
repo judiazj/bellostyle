97dabbba07c3d3834d4b171c0b84f45a119ae205
Judiazj <judiazj@unal.edu.co>
2024-10-07 22:51:47 -0500

    documentacion con swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,17 +2,21 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
   @Post('/register')
+  @ApiBody({ type: CreateUserDto })
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.authService.createUser(createUserDto);
   }
 
   @Post('/login')
+  @ApiBody({ type: LoginUserDto })
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.loginUser(loginUserDto);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bellostyle')
+    .setDescription('this is a ecommerce api')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
